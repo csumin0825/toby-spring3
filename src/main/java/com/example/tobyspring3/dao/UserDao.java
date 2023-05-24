95a1c8b2c87ce8c85699ee86c32685273015219e
sumin <csumin008255@gmail.com>
2023-05-24 14:11:55 +0900

    템플릿 메소드 패턴 적용

--- src/main/java/com/example/tobyspring3/dao/UserDao.java
+++ src/main/java/com/example/tobyspring3/dao/UserDao.java
@@ -7,24 +7,40 @@ import java.util.Map;
 
 import static java.lang.System.getenv;
 
-public class UserDao {
-    public Connection getConnection() throws ClassNotFoundException, SQLException {
-        Map<String, String> env = getenv();
-        String dbHost = env.get("DB_HOST");
-        String dbUser = env.get("DB_USER");
-        String dbPassword = env.get("DB_PASSWORD");
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection conn = DriverManager.getConnection(dbHost, dbUser,dbPassword);
-        return conn;
+public abstract class UserDao {
+    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
+//    {
+//        Map<String, String> env = getenv();
+//        String dbHost = env.get("DB_HOST");
+//        String dbUser = env.get("DB_USER");
+//        String dbPassword = env.get("DB_PASSWORD");
+//        Class.forName("com.mysql.cj.jdbc.Driver");
+//        Connection conn = DriverManager.getConnection(dbHost, dbUser,dbPassword);
+//        return conn;
+//    }
 
-    }
+//    public void add(User user) throws ClassNotFoundException, SQLException {
+//        Connection conn = getConnection();
+//        PreparedStatement pstmt = conn.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+//        pstmt.setString(1,user.getId());
+//        pstmt.setString(2,user.getName());
+//        pstmt.setString(3,user.getPassword());
+//
+//        pstmt.executeUpdate();
+//
+//        pstmt.close();
+//        conn.close();
+//    }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
+
+        // DB 연결
         Connection conn = getConnection();
-        PreparedStatement pstmt = conn.prepareStatement("insert into users(id, name, password) values(?,?,?)");
-        pstmt.setString(1,user.getId());
-        pstmt.setString(2,user.getName());
-        pstmt.setString(3,user.getPassword());
+
+        PreparedStatement pstmt = conn.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
+        pstmt.setString(1, user.getId());
+        pstmt.setString(2, user.getName());
+        pstmt.setString(3, user.getPassword());
 
         pstmt.executeUpdate();
 
@@ -56,15 +72,16 @@ public class UserDao {
     }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao userDao = new UserDao();
+        UserDao userDao = new NUserDao();
         User user = new User();
-        user.setId("3");
+        user.setId("4");
         user.setName("최미미");
         user.setPassword("123456");
         userDao.add(user);
-        User selectedUser = userDao.get("3");
-        System.out.println(selectedUser.getId());
-        System.out.println(selectedUser.getName());
-        System.out.println(selectedUser.getPassword());
+
+//        User selectedUser = userDao.get("1");
+//        System.out.println(selectedUser.getId());
+//        System.out.println(selectedUser.getName());
+//        System.out.println(selectedUser.getPassword());
     }
 }
